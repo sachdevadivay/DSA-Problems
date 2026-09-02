8f8880cbd3dbe5e728889a861f05d79b193adb3f
Divay Sachdeva <sachdevadivay300@gmail.com>
2026-09-03 00:21:18 +0530

    Create Day 2.c

---
+++ Day 2.c
@@ -0,0 +1,22 @@
+# Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+int compare(const void* a, const void* b) {
+    int x = *(const int*)a;
+    int y = *(const int*)b;
+
+    if (x < y) return -1;
+    if (x > y) return 1;
+    return 0;
+}
+
+bool containsDuplicate(int* nums, int numsSize) {
+    qsort(nums, numsSize, sizeof(int), compare);
+
+    for (int i = 0; i < numsSize - 1; i++) {
+        if (nums[i] == nums[i + 1]) {
+            return true;
+        }
+    }
+
+    return false;
+}
