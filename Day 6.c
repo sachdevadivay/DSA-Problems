43789d1a07af3d1f9701f7fc449fe7e9c1203ba2
Divay Sachdeva <sachdevadivay300@gmail.com>
2026-09-07 22:14:53 +0530

    Add removeDuplicates function for unique elements
    Implement function to remove duplicates from sorted array.

---
+++ Day 6.c
@@ -0,0 +1,18 @@
+# Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
+
+Consider the number of unique elements in nums to be k​​​​​​​​​​​​​​. After removing duplicates, return the number of unique elements k.
+
+The first k elements of nums should contain the unique numbers in sorted order. The remaining elements beyond index k - 1 can be ignored.
+
+  int removeDuplicates(int* nums, int numsSize) {
+    int j = 1;
+
+    for (int i = 1; i < numsSize; i++) {
+        if (nums[i] != nums[i - 1]) {
+            nums[j] = nums[i];
+            j++;
+        }
+    }
+
+    return j;
+}
