10609c78bf7106631ae0d2fe21b7d3a81b45e5a7
Divay Sachdeva <sachdevadivay300@gmail.com>
2026-09-12 23:31:35 +0530

    Add palindrome checking function
    Implement function to check if a string is a palindrome by ignoring case and non-alphanumeric characters.

---
+++ Day 11.c
@@ -0,0 +1,28 @@
+# A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+bool isPalindrome(char* s) {
+    int left = 0;
+    int right = strlen(s) - 1;
+
+    while (left < right) {
+        
+        while (left < right && !isalnum(s[left])) {
+            left++;
+        }
+
+        while (left < right && !isalnum(s[right])) {
+            right--;
+        }
+
+        if (tolower(s[left]) != tolower(s[right])) {
+            return false;
+        }
+
+        left++;
+        right--;
+    }
+
+    return true;
+}
